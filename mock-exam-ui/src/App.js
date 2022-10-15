d83c7bf5338ec5279fc8eb3e5579286ca8865ea9
vitalii <vitaliisili@yahoo.com>
2022-10-15 15:09:14 +0200

    test front end

--- mock-exam-ui/src/App.js
+++ mock-exam-ui/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
 function App() {
+
+  useEffect(() => {
+    const url = "http://localhost:9002/api/question";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing branch from ui
-        </a>
+
       </header>
     </div>
   );
