48ab9954d57c56171ea5001e587c609b2dc5d1a0
vitalii <vitaliisili@yahoo.com>
2022-10-15 15:08:25 +0200

    add model layer

--- mock-exam-service/src/main/java/com/mockexam/mockexamservice/model/User.java
+++ mock-exam-service/src/main/java/com/mockexam/mockexamservice/model/User.java
@@ -1,5 +1,38 @@
 package com.mockexam.mockexamservice.model;
 
-public class User {
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
+@Table(name = "USERS")
+public class User extends BaseEntity{
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+
+    private String secondName;
+
+    @ManyToMany(cascade = CascadeType.DETACH)
+    @JoinTable(name = "USERS_ROLE",
+            joinColumns = {@JoinColumn(name = "users_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    @ToString.Exclude
+    private List<Role> roles;
 
 }
