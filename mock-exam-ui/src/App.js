87225e11a6cdad9305400c934410d64601979a22
vitalii <vitaliisili@yahoo.com>
2022-10-19 13:51:26 +0200

    add test page react

--- mock-exam-ui/src/App.js
+++ mock-exam-ui/src/App.js
@@ -1,32 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect} from "react";
+import Question from "./component/question/Question";
 
 function App() {
-
-  useEffect(() => {
-    const url = "http://localhost:9002/api/question";
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(json);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
-    </div>
+    <Question/>
   );
 }
 
