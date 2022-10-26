3b32a37fe9630c916ffe544de8543c0a86ff30fa
vitalii <vitaliisili@yahoo.com>
2022-10-26 22:22:48 +0200

    add navbar

--- mock-exam-ui/src/component/NavBar.js
+++ mock-exam-ui/src/component/NavBar.js
@@ -1,36 +1,127 @@
 import * as React from 'react';
 import styled from "styled-components";
+import { TfiBook } from "react-icons/tfi";
+import {Link} from "react-router-dom";
+import "../fonts/sf/SF-Pro-Display-Bold.ttf"
+import { VscAccount } from "react-icons/vsc";
+import { TbFileInfo } from "react-icons/tb";
+import { GoCommentDiscussion } from "react-icons/go";
+import logo from '../image/logo/logo.png'
+import { TfiPowerOff } from "react-icons/tfi";
+import NavBarSearch from "./NavBarSearch";
 
 const NavBarWrapper = styled.div`
   background-color: #3C3F41;
   width: 100%;
+  height: 60px;
   position: absolute;
   top: 0;
   display: inline-flex;
   justify-content: space-between;
-  padding: 20px 0;
+  align-items: center;
   color: #FFF;
 `
 
 const NavBarLogo = styled.div`
   padding-left: 20px;
+  .logo {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const NavBarSearchWrapper = styled.div`
+  
 `
 
 const NavBarMenu = styled.div`
+  height: 100%;
+`
 
+const NavBarMenuItem = styled.div`
+  font-family: SF-Pro-Display-Bold, sans-serif;
+  font-weight: bold;
+  height: 100%;
+  .menu-icon {
+    font-size: 25px;
+    margin-right: 8px;
+  }
+  .menu-link {
+    text-decoration: none;
+    color: #FFF;
+    display: inline-flex;
+    align-items: center;
+    padding: 0 10px;
+    border-right: 1px solid #525659;
+    height: 100%;
+    width: 120px;
+    justify-content: center;
+    :hover {
+      background-color: #525659;
+      color: #C4C4C4;
+    }
+  }
+  .menu-link:first-child {
+    border-left: 1px solid #525659;
+  }
+  .menu-text {
+    margin-bottom: -3px;
+  }
 `
 
 const NavBarLogout = styled.div`
   padding-right: 20px;
+  .menu-link {
+    text-decoration: none;
+    color: #FFF;
+  }
+  .logout{
+    font-size: 25px;
+    :hover {
+      color: #C4607E;
+    }
+  }
 `
 
 const NavBar = () => {
 
     return (
         <NavBarWrapper>
-            <NavBarLogo>Logo</NavBarLogo>
-            <NavBarMenu>Menu</NavBarMenu>
-            <NavBarLogout>Logout</NavBarLogout>
+            <NavBarLogo>
+                <img className='logo' src={logo} alt="logo"/>
+            </NavBarLogo>
+            <NavBarSearchWrapper>
+                <NavBarSearch/>
+            </NavBarSearchWrapper>
+            <NavBarMenu>
+                <NavBarMenuItem >
+                    <Link className="menu-link" to="/public-exams">
+                        <TfiBook className="menu-icon"></TfiBook>
+                        <span className="menu-text">EXAMS</span>
+                    </Link>
+
+                    <Link className="menu-link" to="/account">
+                        <VscAccount className="menu-icon"></VscAccount>
+                        <span className="menu-text">ACCOUNT</span>
+                    </Link>
+
+                    <Link className="menu-link" to="/about">
+                        <TbFileInfo className="menu-icon"></TbFileInfo>
+                        <span className="menu-text">ABOUT</span>
+                    </Link>
+
+                    <Link className="menu-link" to="/about">
+                        <GoCommentDiscussion className="menu-icon"></GoCommentDiscussion>
+                        <span className="menu-text">CONTACT</span>
+                    </Link>
+
+                </NavBarMenuItem>
+            </NavBarMenu>
+            <NavBarLogout>
+                <Link className="menu-link" to="/logout">
+                    <TfiPowerOff className="logout"></TfiPowerOff>
+                </Link>
+            </NavBarLogout>
         </NavBarWrapper>
     )
 }
---
+++ mock-exam-ui/src/component/NavBarSearch.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import styled from "styled-components";
+import {TfiSearch} from "react-icons/tfi";
+import {useState} from "react";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
+
+const Icon = styled.div`
+  background-color: #F0F0F0;
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom-left-radius: 15px;
+  border-top-left-radius: 15px;
+  margin-right: -2px;
+  .search-icon {
+    color: #3C3F41;
+  }
+`
+
+const Input = styled.input`
+  padding: 4px 5px;
+  height: 22px;
+  width: 260px;
+  border: none;
+  background-color: #F0F0F0;
+`
+
+const SearchBtn = styled.button`
+  text-decoration: none;
+  border: none;
+  -webkit-appearance: button;
+  overflow: visible;
+  text-transform: none;
+  border-bottom-right-radius: 15px;
+  border-top-right-radius: 15px;
+  background-color: #5F6568;
+  color: #F0F0F0;
+  padding: 0 10px;
+  :hover {
+    background-color: #656B6E;
+  }
+`
+
+const NavBarSearch = () => {
+
+    const [searchText, setSearchText] = useState("")
+
+    const searchHandler = () => {
+        console.log(searchText)
+    }
+
+    return (
+        <Wrapper>
+            <Icon><TfiSearch className="search-icon"/></Icon>
+            <Input onChange={(e) => setSearchText(e.target.value)}
+                   value={searchText}
+                   type="text"
+                   placeholder="Search exams "/>
+            <SearchBtn onClick={searchHandler}>Search</SearchBtn>
+        </Wrapper>
+    )
+}
+
+export default NavBarSearch
